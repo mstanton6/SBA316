5a26973d91921a0929a24906245754289ab1a300
mstanton6 <mstanton6@yahoo.com>
2025-12-26 13:40:28 -0500

    Task 3 and 4 complete.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const guess  = document.getElementById('guess');
+const guess = document.getElementById('guess');
 const submitbtn = document.querySelector('#submit');
 const startnewbtn = document.getElementById('startnew');
 const exitbtn = document.getElementById('exit');
@@ -17,32 +17,43 @@ startnewbtn.addEventListener('click', startnew);
 exitbtn.addEventListener('click', exit);
 
 function submitit(event) {
-  event.preventDefault();
- // console.log('The guess ' + Number(guess.value));
+    event.preventDefault();
 
-  if (isNaN(Number(guess.value))){
-    msgbtn.textContent = "That is not a number";
-  } else if(Number(guess.value) === answer){
-    msgbtn.textContent = "You guessed it!";
-  }  else if (Number(guess.value) < answer) {
-    msgbtn.textContent = "Too low";
-  }
-  else {
-    msgbtn.textContent = "Too high";
-  }
+    // *** Use the parent-child-sibling relationship to navigate between elements at least once 
+    let theparentnode = submitbtn.parentNode;
+    console.log('The submit button parent is: ', theparentnode);
 
-  return;
+    // ** Iterate over a collection of elements to accomplish some task. (turn all the buttons color to blue)
+    const buttons = document.getElementsByTagName('button');
+
+    for (const button of buttons) {
+        button.style.color = 'blue';  // turn all the buttons color to blue on submit
+    }
+
+    // This is the game logic
+    if (isNaN(Number(guess.value))) {
+        msgbtn.textContent = "That is not a number";
+    } else if (Number(guess.value) === answer) {
+        msgbtn.textContent = "You guessed it!";
+    } else if (Number(guess.value) < answer) {
+        msgbtn.textContent = "Too low";
+    }
+    else {
+        msgbtn.textContent = "Too high";
+    }
+
+    return;
 }
 
 function startnew() {
- // console.log('Got to the start new function');
-  // Get a new random number number every time "Start New Game" is clicked.
-  answer = Math.round(Math.random() * 100);
- // console.log('answer ' + answer);
-  return;
+    // console.log('Got to the start new function');
+    // Get a new random number number every time "Start New Game" is clicked.
+    answer = Math.round(Math.random() * 100);
+    console.log('answer ' + answer);
+    return;
 }
 
 function exit() {
-  window.close();
-  return;
+    window.close();
+    return;
 }
