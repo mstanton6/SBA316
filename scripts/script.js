71112704195fec32fe4839c41e3dcf8a69853e42
mstanton6 <mstanton6@yahoo.com>
2025-12-26 19:08:36 -0500

    Tasks 5 and 6 and fix misc things.

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,30 @@ const submitbtn = document.querySelector('#submit');
 const startnewbtn = document.getElementById('startnew');
 const exitbtn = document.getElementById('exit');
 const msgbtn = document.getElementById('message');
+const changebtn = document.getElementById('changesubmit');
+const changeinput = document.getElementById('changes');
 // const formbtn = document.getElementById('guessForm');
-// console.log('submitbtn ' + submitbtn.name);
 
 // Get a random number
-let answer = Math.round(Math.random() * 100) + 1;
-console.log('answer ' + answer);
+let answer = Math.floor(Math.random() * 100) + 1;
+// console.log('answer ' + answer);
 
 // Event Handelers
 //guessbtn.addEventListener('click', guessit);
 submitbtn.addEventListener('click', submitit);
 startnewbtn.addEventListener('click', startnew);
 exitbtn.addEventListener('click', exit);
+changebtn.addEventListener('click', changeit);
+
+function changeit(event) {
+    // Modify the HTML or text content of at least one element in response to user interaction using innerHTML,
+    // innerText, or textContent.
+    submitbtn.value = changeinput.value;
+}
 
 function submitit(event) {
     event.preventDefault();
-    
+
     // cache the guess into hisitory
     let history = document.getElementById('historyguess'); // ('guessForm');
     let li = document.createElement("li");
@@ -28,16 +36,16 @@ function submitit(event) {
 
     // *** Use the parent-child-sibling relationship to navigate between elements at least once 
     let theparentnode = submitbtn.parentNode;
-    console.log('The submit button parent is: ', theparentnode);
-    
+    // console.log('The submit button parent is: ', theparentnode);
+
     // get the buttons and the colors for them
     const buttons = document.getElementsByTagName('button');
-    colorarr = ['red','green','blue','purple','yellow','orange','lightblue','cyan','magenta','aqua'];
-    newcolor = Math.round(Math.random() * 10) + 1; // get a random number between 1-10
+    const colorarr = ['red', 'green', 'blue', 'purple', 'DarkGray', 'orange', 'MidnightBlue', 'Gunmetal', 'magenta', 'Navy'];
+    let newcolor = Math.round(Math.random() * colorarr.length); // get a random number between 1-10
 
     // ** Iterate over a collection of elements to accomplish some task. (turn all the buttons color to a randowm color)
     for (const button of buttons) {
-      button.style.color = colorarr[newcolor];  // turn all the buttons color to a randowm color on submit
+        button.style.color = colorarr[newcolor];  // turn all the buttons color to a randowm color on submit
     }
 
     // This is the game logic
@@ -58,16 +66,19 @@ function submitit(event) {
 function startnew() {
 
     // Clear the messages
-    message.textContent = "";
+    msgbtn.textContent = "";
     historyguess.textContent = "";
+    guess.value = "";
+    changeinput.value = "";
+
 
     // Get a new random number number every time "Start New Game" is clicked.
-    answer = Math.round(Math.random() * 100) + 1;
-    console.log('answer ' + answer);
+    answer = Math.floor(Math.random() * 100) + 1;
+    //console.log('answer ' + answer);
 
     // Create at least one element using createElement.
     let container = document.getElementById('guessForm')
-    newbutton = document.createElement('button');
+    let newbutton = document.createElement('button');
     newbutton.style.width = '50px';
     newbutton.style.height = '30px';
     container.appendChild(newbutton);
