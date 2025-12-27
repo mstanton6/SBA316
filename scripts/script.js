4a3ed787861a95fd017f4b1bc6f986bf461499c0
mstanton6 <mstanton6@yahoo.com>
2025-12-27 14:48:39 -0500

    Through task 13 (skipped 7,9,12)

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,24 @@
+// 1. Requirement Cache at least one element using selectElementById.
 const guess = document.getElementById('guess');
+// 2. Requirement Cache at least one element using querySelector
 const submitbtn = document.querySelector('#submit');
 const startnewbtn = document.getElementById('startnew');
 const exitbtn = document.getElementById('exit');
 const msgbtn = document.getElementById('message');
 const changebtn = document.getElementById('changesubmit');
 const changeinput = document.getElementById('changes');
+const history = document.getElementById('historyguess'); // ('guessForm');
+const overviewbtn = document.getElementById('overview');
 // const formbtn = document.getElementById('guessForm');
+
+ overviewbtn.textContent = `A number guessing game is a simple challenge where a player tries to guess a secret number chosen by the computer or another player within a limited number of attempts, receiving "too high" or "too low" hints after each incorrect guess to narrow down the possibilities and find the answer efficiently.`;
 
 // Get a random number
 let answer = Math.floor(Math.random() * 100) + 1;
 // console.log('answer ' + answer);
 
-// Event Handelers
+// Event listeners
+// 11. Requirement - Register at least two different event listeners and create the associated event handler functions.
 //guessbtn.addEventListener('click', guessit);
 submitbtn.addEventListener('click', submitit);
 startnewbtn.addEventListener('click', startnew);
@@ -19,31 +26,34 @@ exitbtn.addEventListener('click', exit);
 changebtn.addEventListener('click', changeit);
 
 function changeit(event) {
-    // Modify the HTML or text content of at least one element in response to user interaction using innerHTML,
-    // innerText, or textContent.
-    submitbtn.value = changeinput.value;
+    // 10. Requirement - Modify at least one attribute of an element in response to user interaction.
+    let changevalue = changeinput.value;
+    submitbtn.value = changevalue;
+    changeinput.value = "";
 }
 
 function submitit(event) {
     event.preventDefault();
 
+    // 8. Requirement - Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+     overviewbtn.textContent = "This text has now been changed."
+
     // cache the guess into hisitory
-    let history = document.getElementById('historyguess'); // ('guessForm');
     let li = document.createElement("li");
     li.textContent = guess.value;
-    // Use appendChild and/or prepend to add new elements to the DOM.
+    // 6. Requirement - Use appendChild and/or prepend to add new elements to the DOM.
     history.prepend(li);
 
-    // *** Use the parent-child-sibling relationship to navigate between elements at least once 
+    // 3. Requirement Use the parent-child-sibling relationship to navigate between elements at least once 
     let theparentnode = submitbtn.parentNode;
     // console.log('The submit button parent is: ', theparentnode);
 
     // get the buttons and the colors for them
     const buttons = document.getElementsByTagName('button');
     const colorarr = ['red', 'green', 'blue', 'purple', 'DarkGray', 'orange', 'MidnightBlue', 'Gunmetal', 'magenta', 'Navy'];
-    let newcolor = Math.round(Math.random() * colorarr.length); // get a random number between 1-10
+    let newcolor = Math.floor(Math.random() * colorarr.length); // get a random number between 1-10
 
-    // ** Iterate over a collection of elements to accomplish some task. (turn all the buttons color to a randowm color)
+    // ** 4. Requirement - Iterate over a collection of elements to accomplish some task. (turn all the buttons color to a randowm color)
     for (const button of buttons) {
         button.style.color = colorarr[newcolor];  // turn all the buttons color to a randowm color on submit
     }
@@ -60,6 +70,15 @@ function submitit(event) {
         msgbtn.textContent = "Too high";
     }
 
+//     let guessnum = Number(guess.value);
+//    // console.log('Number.isInteger(guess.value) ' + Number.isInteger(guess.value));
+//     if (!Number.isInteger(guessnum))  {
+//         console.log('got here');
+//         msgbtn.textContent = "Please enter an integer value."
+//     };
+
+    guess.value = "";
+
     return;
 }
 
@@ -67,7 +86,7 @@ function startnew() {
 
     // Clear the messages
     msgbtn.textContent = "";
-    historyguess.textContent = "";
+    history.textContent = "";
     guess.value = "";
     changeinput.value = "";
 
@@ -76,8 +95,8 @@ function startnew() {
     answer = Math.floor(Math.random() * 100) + 1;
     //console.log('answer ' + answer);
 
-    // Create at least one element using createElement.
     let container = document.getElementById('guessForm')
+    // 5. Requirement - Create at least one element using createElement.
     let newbutton = document.createElement('button');
     newbutton.style.width = '50px';
     newbutton.style.height = '30px';
